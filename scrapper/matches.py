193fbfaffd226844670f8dd9f7b0efe47effc787
KishanAdhikari <adhikarykishan11@gmail.com>
2023-11-02 21:31:12 +0545

    Recent matches and link fetched

--- scrapper/matches.py
+++ scrapper/matches.py
@@ -1,12 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
-import psycopg2
-from constant.urls import base_url
 
-def get_response(url):
-    matches=[]
-    response=requests.get(url)
-    soup=BeautifulSoup(response.text,'html.parser')
-    return soup.prettify
+base_url = "https://www.cricbuzz.com/cricket-match/live-scores"
 
-print(get_response(base_url))
+def get_soup(url):
+    # Set the user-agent header
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
+
+def extract_match_links(soup):
+    match_links = {}
+
+    # Find all match elements
+    match_elements = soup.find_all("div", class_="cb-col-100 cb-col cb-schdl")
+
+    for match in match_elements:
+        link = match.find("a")
+        title = link['title']
+        href = 'https://www.cricbuzz.com' + link['href']
+        match_links[title] = href
+
+    return match_links
+
+
+def get_matchlink():
+    soup = get_soup(base_url)
+    matchlink_map = extract_match_links(soup)
+    return matchlink_map
+
+    
+       
+
+
+
+    
+
+
+
