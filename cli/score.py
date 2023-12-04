913c6b5a9146db5349d382e3190a9e6e6a419b49
Kishan <adhikarykishan11@gmail.com>
2023-12-04 17:57:55 +0545

    result in tabular format

--- cli/score.py
+++ cli/score.py
@@ -1,27 +1,38 @@
 import click
 from scrapper.matches import Match
 from rich.console import Console
+from rich.table import Table
 
 console=Console()
 
 
 @click.command()
+def lmatches():
+    match_info=Match().get_matchlink()
+    console.print("\t\tLive Matches")
+    for match in match_info:
+        console.print(f'🏏[bold cyan]{match["tournament"]}[/bold cyan]')
+        console.print(f'{match["team1"]} vs {match["team2"]}\n\n')
+    
+
+@click.command()
 def lscore():
     match_inform=Match().get_matchlink()
-   
-    print("\t\tLive Matches")
     for match_info in  match_inform:
-        console.print(f'[green]Title: {match_info["title"]}[/green]')
-        console.print(f'[blue]Team 1: {match_info["team1"]}, Score: {match_info["team1_score"]}[/blue]')
-        console.print(f'Team 2: {match_info["team2"]}, Score: {match_info["team2_score"]}')
-        console.print(f'Result: {match_info["result"]}')
-        console.print(f'Status: {match_info["status"]}')
-        console.print(f'Tournament: {match_info["tournament"]}')
-        print(f'Link: {match_info["link"]}\n')
-    
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Title", style="cyan", no_wrap=True)
+        table.add_column(match_info['title'], justify="right", style="green")
+        table.add_row(match_info['team1'],match_info['team1_score'])
+        table.add_row(match_info['team2'],match_info['team2_score'])
+        table.add_row('Result',match_info['result'])
+        console.print(table)
+
+
+
 @click.group()
 def cli():
     pass
 cli.add_command(lscore)
+cli.add_command(lmatches)
 if __name__ == '__main__':
     cli()
